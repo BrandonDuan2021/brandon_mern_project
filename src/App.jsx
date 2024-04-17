a1538f7045d4b69f2c1204bc179c1664294280e5
brandon <bizzarebeta@gmail.com>
2024-04-17 18:08:36 -0400

    Added Routers and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './assets/pages/Home';
+import Profile from './assets/pages/Profile';
+import SignOut from './assets/pages/SignUp';
 
 export default function App() {
   return (
-    <div className='text-red-500'>
-      wut
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/sign-in" element={<SignIn/>}/>
+        <Route path="/sign-up" element={<SignUp/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
