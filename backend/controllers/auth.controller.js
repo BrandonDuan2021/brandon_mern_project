6b318c2275052c7d36a165baf6ebec00091c1e0d
brandon <bizzarebeta@gmail.com>
2024-05-03 19:33:41 -0400

    Create sign in API route and functions

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,21 +1,42 @@
 import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
 import bcrytjs from 'bcryptjs';
+import jwt from "jsonwebtoken";
 
 export const signUp = async (req, res, next)=>{
     try{
-    console.log(req.body)
-    const { username, email, password } = req.body;
-    const hashedPwd = bcrytjs.hashSync(password, 10)
-    const newUser = new User({
-        username,
-        email,
-        password: hashedPwd
-    })
-        await newUser.save()
-        res.status(200).json('User created successfully!')
+        const { username, email, password } = req.body;
+        const hashedPwd = bcrytjs.hashSync(password, 10)
+        const newUser = new User({
+            username,
+            email,
+            password: hashedPwd
+        })
+            await newUser.save()
+            res.status(200).json('User created successfully!')
     } catch(e){
         next(e)
     }
-    
+
+}
+
+export const signIn = async (req, res, next)=>{
+    try{
+        const { email, password } = req.body;
+        console.log(req.body)
+        const validUser = await User.findOne({ email }) 
+        if (! validUser) return next(errorHandler(404, "User is not found."))
+        const validPassword = bcrytjs.compareSync(password, validUser.password)
+        if (!validPassword) return next(errorHandler(401, "The email and password you provided is not correct. Please try again."))
+        const {password: pass, ...rest} = validUser._doc
+        const token = jwt.sign({
+            id: validUser._id
+        },
+        process.env.JWT_SECRET_KEY)
+
+        res.cookie('token', token).status(200).json(rest)
+    } catch(e){
+        next(e)
+    }
 
 }
--- backend/utils/error.js
+++ backend/utils/error.js
@@ -2,5 +2,6 @@ export const errorHandler = (statusCode, msg) => {
     const error = new Error()
     error.statusCode = statusCode
     error.message = msg
+    console.log(error)
     return error
 }
